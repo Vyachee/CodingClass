34c01d6f1a34ee49ae409213c2bf3bd433835c41
Vyacheslav <38556725+Vyachee@users.noreply.github.com>
2020-05-01 17:41:27 +0500

    test5

---
+++ vkapptest/precache-manifest.20fbb69ab9fd95983da902fc7e1d5b13.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0500f465aa34580332d1cee598bd84c3",
+    "url": "./index.html"
+  },
+  {
+    "revision": "e670d31811689bfb3b7a",
+    "url": "./static/css/2.86df01db.chunk.css"
+  },
+  {
+    "revision": "101946d73e180a26f94e",
+    "url": "./static/css/main.cfa2298b.chunk.css"
+  },
+  {
+    "revision": "e670d31811689bfb3b7a",
+    "url": "./static/js/2.22e2aa88.chunk.js"
+  },
+  {
+    "revision": "21ced859ea2b2d6b856d461ad6c2afed",
+    "url": "./static/js/2.22e2aa88.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "101946d73e180a26f94e",
+    "url": "./static/js/main.28a1fb9b.chunk.js"
+  },
+  {
+    "revision": "4acf0c9b94a9ef9a1dfb",
+    "url": "./static/js/runtime-main.66cbc036.js"
+  },
+  {
+    "revision": "4e1ec8403d903dc514271d7328fbdeb3",
+    "url": "./static/media/persik.4e1ec840.png"
+  },
+  {
+    "revision": "b99e8ee27ac446e7f85fd8bb73d72aae",
+    "url": "./static/media/plus.b99e8ee2.svg"
+  }
+]);
--- vkapptest/service-worker.js
+++ vkapptest/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.922b3899e7c8c71eded53eb5aceee883.js"
+  "./precache-manifest.20fbb69ab9fd95983da902fc7e1d5b13.js"
 );
 
 self.addEventListener('message', (event) => {
