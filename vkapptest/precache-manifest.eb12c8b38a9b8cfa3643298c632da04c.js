399a0030339340b137b1f5213c9a0f5f440497d5
Vyacheslav <38556725+Vyachee@users.noreply.github.com>
2020-05-01 11:52:54 +0500

    vkapptest

---
+++ vkapptest/precache-manifest.eb12c8b38a9b8cfa3643298c632da04c.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "149fdbc18cc0538841eefb538ee11254",
+    "url": "/index.html"
+  },
+  {
+    "revision": "27041dc5d6a7474261e1",
+    "url": "/static/css/2.86df01db.chunk.css"
+  },
+  {
+    "revision": "801dc8348ca01a29daeb",
+    "url": "/static/css/main.981f7211.chunk.css"
+  },
+  {
+    "revision": "27041dc5d6a7474261e1",
+    "url": "/static/js/2.8d7a1ac4.chunk.js"
+  },
+  {
+    "revision": "21ced859ea2b2d6b856d461ad6c2afed",
+    "url": "/static/js/2.8d7a1ac4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "801dc8348ca01a29daeb",
+    "url": "/static/js/main.5d161741.chunk.js"
+  },
+  {
+    "revision": "91bd7b3579655a4d168c",
+    "url": "/static/js/runtime-main.89ffa98d.js"
+  },
+  {
+    "revision": "4e1ec8403d903dc514271d7328fbdeb3",
+    "url": "/static/media/persik.4e1ec840.png"
+  }
+]);
